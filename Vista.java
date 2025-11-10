d06163a1ce18d174446d13cdeeff396f473feb31
SpivaR07 <pivaralsebastian81@gmail.com>
2025-11-10 16:59:49 -0600

    Implement printMenu method in Vista class

--- Vista.java
+++ Vista.java
@@ -1,4 +1,13 @@
 public class Vista {
 
-    public void printMenu() {}
+    public void printMenu() {
+        System.out.println("===== MEDIA MANAGER =====");
+        System.out.println("1. Ver contenido");
+        System.out.println("2. Agregar contenido");
+        System.out.println("3. Eliminar contenido");
+        System.out.println("4. Buscar contenido");
+        System.out.println("5. Generar reporte");
+        System.out.println("6. Salir");
+        System.out.println("=========================");
+    }
 }
