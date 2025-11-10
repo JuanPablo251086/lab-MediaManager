88faaf89e6945af30a8f70fd4c89430dd9f7cd8a
SpivaR07 <pivaralsebastian81@gmail.com>
2025-11-10 17:15:30 -0600

    Enhance Video class with playback and properties methods

--- Video.java
+++ Video.java
@@ -1,28 +1,89 @@
 public class Video extends Content implements IAudio, IPlayable, IDisplayable {
     private String calidad;
-
-    public Video() {}
-
-    public void Play() {}
-
+    private double duracion;
+    private boolean reproduciendo;
+    private boolean publicado;
+    private int tiempoActual;
+    
+    public Video() {
+        super();
+        this.reproduciendo = false;
+        this.publicado = false;
+        this.tiempoActual = 0;
+    }
+    
     @Override
-    public void modificarVolumen() {}
-
+    public void play() {
+        this.reproduciendo = true;
+    }
+    
     @Override
-    public void cambiarVelocidad() {}
-
+    public void stop() {
+        this.reproduciendo = false;
+        this.tiempoActual = 0;
+    }
+    
+    public void pause() {
+        this.reproduciendo = false;
+    }
+    
     @Override
-    public void play() {}
-
+    public String getVideoProperties() {
+        return "Calidad: " + calidad + ", Género: " + getGenere() + ", Duración: " + duracion + " min";
+    }
+    
     @Override
-    public void stop() {}
-
+    public String contenido() {
+        return "Video - Calidad: " + calidad + " - Duración: " + duracion + " min - Estado: " + (reproduciendo ? "Reproduciendo" : "Detenido");
+    }
+    
     @Override
-    public String getVideoProperties() { return ""; }
-
+    public void eliminar() {
+        this.stop();
+        this.publicado = false;
+        this.calidad = null;
+        this.duracion = 0;
+    }
+    
     @Override
-    public void mostrar() {}
-
+    public void publicar() {
+        this.publicado = true;
+    }
+    
     @Override
-    public int getLargo() { return 0; }
+    public void visualizar() {
+        this.play();
+    }
+    
+    public String getCalidad() {
+        return calidad;
+    }
+    
+    public void setCalidad(String calidad) {
+        this.calidad = calidad;
+    }
+    
+    public double getDuracion() {
+        return duracion;
+    }
+    
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+    
+    public boolean isReproduciendo() {
+        return reproduciendo;
+    }
+    
+    public boolean isPublicado() {
+        return publicado;
+    }
+    
+    public int getTiempoActual() {
+        return tiempoActual;
+    }
+    
+    public void setTiempoActual(int tiempoActual) {
+        this.tiempoActual = tiempoActual;
+    }
 }
