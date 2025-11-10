23a0a52479a175ed06889ad98cf47235d0a74b98
SpivaR07 <pivaralsebastian81@gmail.com>
2025-11-10 17:02:16 -0600

    Enhance Imagen class with methods and constructor
    Added constructor and implemented methods for zooming, content retrieval, deletion, publishing, and visualization.

--- Imagen.java
+++ Imagen.java
@@ -1,5 +1,39 @@
 public class Imagen extends Content {
     private String nombre;
 
-    public void zoomear(int nivel) {}
+    public Imagen() {
+        super();
+    }
+
+    public void zoomear(int nivel) {
+        System.out.println("Haciendo zoom al nivel: " + nivel);
+    }
+
+    @Override
+    public String contenido() {
+        return "Imagen: " + nombre;
+    }
+    
+    @Override
+    public String eliminar() {
+        return "Eliminando imagen: " + nombre;
+    }
+    
+    @Override
+    public String publicar() {
+        return "Publicando imagen: " + nombre;
+    }
+    
+    @Override
+    public String visualizar() {
+        return "Visualizando imagen: " + nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
