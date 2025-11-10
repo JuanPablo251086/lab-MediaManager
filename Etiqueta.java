bbb958c278c8c4f9d9a330b743801f73348ea140
SpivaR07 <pivaralsebastian81@gmail.com>
2025-11-10 17:23:35 -0600

    Add active state management to Etiqueta class

--- Etiqueta.java
+++ Etiqueta.java
@@ -1,5 +1,46 @@
 public class Etiqueta {
     private String nombre;
-
-    public void getEtiqueta() {}
+    private boolean activa;
+    
+    public Etiqueta() {
+        super();
+        this.activa = true;
+    }
+    
+    public void getEtiqueta() {
+        // Retorna información de la etiqueta a través del getter
+    }
+    
+    @Override
+    public String contenido() {
+        return "Etiqueta: " + nombre;
+    }
+    
+    @Override
+    public void eliminar() {
+        this.activa = false;
+        this.nombre = null;
+    }
+    
+    @Override
+    public void publicar() {
+        this.activa = true;
+    }
+    
+    @Override
+    public void visualizar() {
+        // La visualización se hace a través del método contenido()
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public boolean isActiva() {
+        return activa;
+    }
 }
