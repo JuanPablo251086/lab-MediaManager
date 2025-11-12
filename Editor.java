ec43b9fdd9ae7d7f7cc47af554dfcbe24fa5c72a
SpivaR07 <pivaralsebastian81@gmail.com>
2025-11-11 19:47:29 -0600

    Implement Editor class with creation and editing methods

--- Editor.java
+++ Editor.java
@@ -1,8 +1,24 @@
 public class Editor extends Usuario {
+    private Content contenidoEnEdicion;
+    private boolean puedeCrear;
+    private int contadorCreaciones;
+    
+    public Editor() {
+        super(0, "", "", 0);
+        this.puedeCrear = true;
+        this.contadorCreaciones = 0;
+    }
 
-    public Editor() {}
+    public void crear() {
+        if (puedeCrear) {
+            // Crear nuevo contenido
+            contadorCreaciones++;
+        }
+    }
 
-    public void crear() {}
-
-    public void editar() {}
+    public void editar() {
+        if (contenidoEnEdicion != null) {
+            // Editar el contenido asignado
+        }
+    }
 }
